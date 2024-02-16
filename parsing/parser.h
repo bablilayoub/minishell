617cd99e5f88704acbfbb4c8571545e9d035567a
Ayoub Bablil <ayoubbablil@gmail.com>
2024-02-16 12:05:27 +0100

    New update

--- parsing/parser.h
+++ parsing/parser.h
@@ -1,38 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abablil <abablil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/15 09:55:30 by abablil           #+#    #+#             */
-/*   Updated: 2024/02/16 12:05:06 by abablil          ###   ########.fr       */
+/*   Created: 2024/02/16 12:02:17 by abablil           #+#    #+#             */
+/*   Updated: 2024/02/16 12:02:29 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef PARSER_H
+# define PARSER_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "libft/libft.h"
-#include "parsing/parser.h"
-#include "execution/execution.h"
-
-typedef struct s_history
-{
-	int					id;
-	char				*cmd;
-	struct s_history	*next;
-	struct s_history	*prev;
-}				t_history;
-
-typedef struct s_data
-{
-	
-}				t_data;
 
 #endif
