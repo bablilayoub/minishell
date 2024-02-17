5c061c46889d67d30664b26fb701ab1d8f6b83aa
alaalalm <achrfalalix@gmail.com>
2024-02-17 19:40:40 +0100

    New push

--- execution/execution.c
+++ execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: alaalalm <alaalalm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 12:33:23 by alaalalm          #+#    #+#             */
-/*   Updated: 2024/02/17 19:20:01 by alaalalm         ###   ########.fr       */
+/*   Updated: 2024/02/17 19:40:23 by alaalalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,18 @@ void start_execution(t_cmd *cmd)
     if (pid == 0)
     {
         close(fd[0]);
-        // dup2(fd[1], 1);
-        // close(fd[1]);
+        dup2(fd[1], 1);
+        close(fd[1]);
         execve(cmd->path, cmd->arguments, NULL);
     }
-    // else
-    // {
-    //     close(fd[1]);
-    //     dup2(fd[0], 0);
-    //     close(fd[0]);
-    //     execve(cmd->path, cmd->args, NULL);
-    // }
+    else
+    {
+        waitpid(pid, NULL, 0);
+        close(fd[1]);
+        dup2(fd[0], 0);
+        close(fd[0]);
+        execve("/usr/bin/wc", (char *[]){"wc", "-l"}, NULL);
+    }
 }
 
 
@@ -56,13 +57,10 @@ void executer(t_data *data)
     while (list_cmd->args != NULL)
     {
         argv[i] = list_cmd->args->arg;
-        printf("%s\n", list_cmd->cmd);
-        printf("%s\n", argv[i]);
         i++;
         list_cmd->args = list_cmd->args->next;
     }
     argv[i] = NULL;
     list_cmd->arguments = argv;
-    // print_commands(list_cmd);
-    // start_execution(list_cmd);
+    start_execution(list_cmd);
 }
