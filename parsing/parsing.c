ea82a33fc7fca99a33b2e33542591680bc1be778
Ayoub Bablil <ayoubbablil@gmail.com>
2024-02-17 19:21:33 +0100

    Fixed

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: abablil <abablil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 12:31:53 by abablil           #+#    #+#             */
-/*   Updated: 2024/02/17 19:15:01 by abablil          ###   ########.fr       */
+/*   Updated: 2024/02/17 19:21:27 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,6 +60,7 @@ void reader(t_data *data)
 		if (!line)
 			break;
 		parser(line, data);
+		executer(data);
 		if (ft_strlen(line) > 0)
 			add_history(line);
 		free(line);
