93ded0c457485fb2b2cf18b2b9d2d476ddc3cc5b
Ayoub Bablil <ayoubbablil@gmail.com>
2024-02-18 13:20:59 +0100

    New push

--- parsing/converter/converter.c
+++ parsing/converter/converter.c
@@ -6,7 +6,7 @@
 /*   By: abablil <abablil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 14:42:27 by abablil           #+#    #+#             */
-/*   Updated: 2024/02/17 23:26:49 by abablil          ###   ########.fr       */
+/*   Updated: 2024/02/18 13:20:01 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,32 +46,6 @@ void convert_tokens_to_commands(t_data *data)
 			head = add_cmd(head, cmd);
 			found_cmd = 1;
 			find_args(cmd, tmp->next);
-		}
-		else if (ft_strncmp(tmp->type, PIPE_LINE, 1) == 0 
-				|| ft_strncmp(tmp->type, HERE_DOC, 2) == 0 
-				|| ft_strncmp(tmp->type, APPEND_OUT, 2) == 0
-				|| ft_strncmp(tmp->type, REDIR_IN, 1) == 0
-				|| ft_strncmp(tmp->type, REDIR_OUT, 1) == 0)
-		{
-			cmd = new_cmd(tmp);
-			found_cmd = 0;
-			if (ft_strncmp(tmp->type, PIPE_LINE, 1) == 0)
-				cmd->redirect = ft_strdup(PIPE_LINE);
-			else if (ft_strncmp(tmp->type, REDIR_IN, 1) == 0)
-				cmd->redirect = ft_strdup(REDIR_IN);
-			else if (ft_strncmp(tmp->type, REDIR_OUT, 1) == 0)
-				cmd->redirect = ft_strdup(REDIR_OUT);
-			else if (ft_strncmp(tmp->type, HERE_DOC, 2) == 0)
-				cmd->redirect = ft_strdup(HERE_DOC);
-			else if (ft_strncmp(tmp->type, APPEND_OUT, 2) == 0)
-				cmd->redirect = ft_strdup(APPEND_OUT);
-			if (tmp->next && tmp->next->next && ft_strncmp(tmp->next->next->type, WORD, 4) == 0)
-			{
-				cmd->output_file = ft_strdup(tmp->next->next->value);
-				tmp = tmp->next->next;
-			}
-			cmd->args = NULL;
-			head = add_cmd(head, cmd);
 		}
 		tmp = tmp->next;
 	}
