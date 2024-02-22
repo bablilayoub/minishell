ff06a88513e899c3ae7e5644f67cedeab18a33e7
Ayoub Bablil <ayoubbablil@gmail.com>
2024-02-23 00:04:03 +0100

    Fixed some bugs

--- parsing/converter/arguments.c
+++ parsing/converter/arguments.c
@@ -6,7 +6,7 @@
 /*   By: abablil <abablil@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 15:45:48 by abablil           #+#    #+#             */
-/*   Updated: 2024/02/22 18:49:49 by abablil          ###   ########.fr       */
+/*   Updated: 2024/02/23 00:01:02 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,13 @@ bool between_dquotes(t_token *token)
 	return (count % 2 == 0);
 }
 
-
 t_token *find_args(t_cmd *cmd, t_token *token)
 {
 	if (!token || !cmd)
 		return (NULL);
 	t_token *tmp = token;
 	t_arg *head = NULL;
+	int found_special_char = 0;
 
 	head = add_arg(head, cmd->cmd, 1);
 	tmp = skip_white_spaces(tmp);
@@ -66,35 +66,35 @@ t_token *find_args(t_cmd *cmd, t_token *token)
 		if (ft_strncmp(tmp->type, PIPE_LINE, 1) == 0 && tmp->state != IN_DQUOTE && tmp->state != IN_QUOTE)
 			break;
 		if (ft_strncmp(tmp->type, WORD, 4) == 0)
+		{
 			head = add_arg(head, tmp->value, 1);
+		}
 		else if (ft_strncmp(tmp->type, ENV, 1) == 0)
 			head = add_arg(head, tmp->value, (tmp->state == IN_QUOTE));
-		else if (ft_strncmp(tmp->type, REDIR_IN, 1) == 0
-			|| ft_strncmp(tmp->type, REDIR_OUT, 1) == 0
-			|| ft_strncmp(tmp->type, APPEND_OUT, 2) == 0
-			|| ft_strncmp(tmp->type, HERE_DOC, 2) == 0)
+		else if ((ft_strncmp(tmp->type, REDIR_IN, 1) == 0 || ft_strncmp(tmp->type, REDIR_OUT, 1) == 0 || ft_strncmp(tmp->type, APPEND_OUT, 2) == 0 || ft_strncmp(tmp->type, HERE_DOC, 2) == 0) && (tmp->state != IN_QUOTE && tmp->state != IN_DQUOTE))
 		{
 			cmd->redirect = tmp->type;
 			if (tmp->next)
 				tmp = skip_white_spaces(tmp->next);
 			if (tmp && ft_strncmp(tmp->type, WORD, 4) == 0)
 				cmd->file = tmp->value;
-			else if (tmp && 
-				(ft_strncmp(tmp->type, REDIR_IN, 1) == 0 
-				|| ft_strncmp(tmp->type, REDIR_OUT, 1) == 0))
+			else if (tmp &&
+					 (ft_strncmp(tmp->type, REDIR_IN, 1) == 0 || ft_strncmp(tmp->type, REDIR_OUT, 1) == 0))
 			{
 				printf("%s\n", PREFIX_ERROR "Syntax error");
 				return (NULL);
-			} 
+			}
 			else
 				cmd->file = NULL;
 		}
+		else if ((ft_strncmp(tmp->type, REDIR_IN, 1) == 0 || ft_strncmp(tmp->type, REDIR_OUT, 1) == 0 || ft_strncmp(tmp->type, APPEND_OUT, 2) == 0 || ft_strncmp(tmp->type, HERE_DOC, 2) == 0) && (tmp->state == IN_QUOTE || tmp->state == IN_DQUOTE))
+			head = add_arg(head, tmp->value, 1);
 		else if (ft_strncmp(tmp->type, WHITE_SPACE, 1) == 0 && (tmp->state == IN_QUOTE || tmp->state == IN_DQUOTE))
 			head = add_arg(head, tmp->value, 1);
 		else if (ft_strncmp(tmp->type, WHITE_SPACE, 1) == 0 && (tmp->state != IN_QUOTE && tmp->state != IN_DQUOTE))
 		{
 			tmp = skip_white_spaces(tmp);
-			if (tmp)
+			if (tmp && not_a_shell_command(tmp))
 				head = add_arg(head, " ", 1);
 			continue;
 		}
