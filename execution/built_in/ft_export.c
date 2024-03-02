e228ec62360849f7573d928c63c3aa46c808976b
alaalalm <achrfalalix@gmail.com>
2024-03-02 01:47:03 +0100

    zahiya

--- execution/built_in/ft_export.c
+++ execution/built_in/ft_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_export.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abablil <abablil@student.42.fr>            +#+  +:+       +#+        */
+/*   By: alaalalm <alaalalm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 14:46:52 by alaalalm          #+#    #+#             */
-/*   Updated: 2024/02/28 21:02:59 by abablil          ###   ########.fr       */
+/*   Updated: 2024/03/02 01:42:19 by alaalalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,54 +47,57 @@ char **load_key_value(char *key, char *value)
     return key_value;
 }
 
-// char *check_dollar_sign(char **env, char *exported)
-// {
-//     int i;
-//     int k;
-//     int j;
-//     char *temp;
-//     char *value;
+char *check_dollar_sign(char **env, char *value)
+{
+    int i;
+    int k;
+    int j;
+    char *tmp;
+    char *var;
+    char *rest;
 
-//     i = 0;
-//     while (exported[i])
-//     {
-//         if (exported[i] == '$')
-//         {
-//             value = ft_substr(exported, 0, i);
-//             k = i;
-//             while (exported[k] && (exported[k] != ' ' && exported[k] != '='))
-//                 k++;
-//             temp = ft_substr(exported, i, k);
-//             if (!temp)
-//                 exit(EXIT_FAILURE);
-//             j = 0;
-//             while (env[j])
-//             {
-//                 if (ft_strncmp(env[j], temp, ft_strlen(temp)) == 0)
-//                     value = ft_strjoin(value, env[j] + ft_strlen(temp));
-//                 j++;
-//             }
-//             if (!env[j])
-//             {
-//                 printf(PREFIX_ERROR "export: not found\n");
-//                 free(temp);
-//                 exit(EXIT_FAILURE);
-//             }
-//         }
-//         i++;
-//     }
-//     return value;
-// }
+    i = -1;
+
+    while (value[++i])
+    {
+        if (value[i] == '$')
+        {
+            k = i;
+            tmp = ft_substr(value, 0, i);                                        
+            while (value[k] && value[k] != ' ' && value[k] != '=')
+                k++;
+            if (value[k] == '=' && value[k + 1] == '\0')
+                rest = ft_strdup("=");
+            else
+                rest = ft_strdup(value + k);
+            var = ft_substr(value, i + 1, k - i - 1);
+            printf("var: %s\n", var);
+            j = -1;
+            while (env[++j])
+            {
+                if (ft_strncmp(env[j], var, ft_strlen(var)) == 0)
+                {
+                    value = ft_strjoin(tmp, ft_getenv(var, env));
+                    value = ft_strjoin(value, rest);
+                    i = -1;
+                    break;
+                }
+                else
+                    value = ft_strjoin(tmp, rest);
+            }
+        }   
+    }
+    return value;
+}
 char **key_value(char *exported, char **env)
 {
-    (void)env;
     char *value;
     char *key;
     char **key_value;
     int i;
     int k;
-
     i = 0;
+
     while (exported[i] && exported[i] != '=')
         i++;
     key = ft_substr(exported, 0, i);
@@ -104,7 +107,8 @@ char **key_value(char *exported, char **env)
     while (exported[k])
         k++;
     value = ft_substr(exported, i + 1, k);
-    // value = check_dollar_sign(env, value);
+    if (ft_strchr(value, '$'))
+        value = check_dollar_sign(env, value);
     if (!value)
     {
         free(key);
@@ -155,10 +159,16 @@ void ft_export(t_data *data, char **env)
     int found;
     size_t k;
     int i;
+    char *tmp;
+    char *exported;
 
+    tmp = ft_strdup("");
     if (!check_exported(data->cmd->arguments[1]))
         ret_same_env(data->env);
-    key_val = key_value(data->cmd->arguments[1], env);
+    i = 0;
+    while (data->cmd->arguments[++i])
+        tmp = ft_strjoin(tmp, data->cmd->arguments[i]);
+    key_val = key_value(tmp, env);
     fd_out = open("export.txt", O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0644);
     if (fd_out == -1)
         return check_error(fd_out, "open", 0);
@@ -171,9 +181,11 @@ void ft_export(t_data *data, char **env)
         if ((ft_strncmp(env[i], key_val[0], k) == 0) && (k == ft_strlen(key_val[0])))
             update_key_value(fd_out, key_val[0], key_val[1], &found);
         else
-            write(fd_out, env[i], ft_strlen(env[i]));
+             write(fd_out, env[i], ft_strlen(env[i]));
         write(fd_out, "\n", 1);
     }
-    add_variable(fd_out, data->cmd->arguments[1], &found);
+    exported = ft_strjoin(key_val[0], "=");
+    exported = ft_strjoin(exported, key_val[1]);
+    add_variable(fd_out, exported, &found);
     close(fd_out);
 }
